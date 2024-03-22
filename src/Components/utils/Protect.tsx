add4b400f86376c0c0d06525eb24c7d8b9ec5efd
Wiktor Tarnowski <MrBlack1223@wp.pl>
2024-03-22 18:57:57 +0100

    Changing logged in condition

--- src/Components/utils/Protect.tsx
+++ src/Components/utils/Protect.tsx
@@ -3,7 +3,7 @@ import { ProtectectionComponentProps } from "../../types"
     
 export const Protect = (props: ProtectectionComponentProps) =>{
     const isLoggedin = localStorage.getItem("refreshToken")
-    if(isLoggedin){
+    if(!isLoggedin){
         return <Navigate to = {props.route} replace />
     }
     return props.child ? props.child : <Outlet />
