7c892fdda5251414e86a4297924b686db675e69c
Wiktor Tarnowski <MrBlack1223@wp.pl>
2024-03-22 18:48:22 +0100

    Changing logged in condition

--- src/Components/utils/Protect.tsx
+++ src/Components/utils/Protect.tsx
@@ -1,11 +1,9 @@
-import { useSelector } from "react-redux"
 import { Navigate, Outlet } from "react-router-dom"
-import { RootState } from "../../Redux/store"
 import { ProtectectionComponentProps } from "../../types"
     
 export const Protect = (props: ProtectectionComponentProps) =>{
-    const user = useSelector((state:RootState) => state.user._id)
-    if(user.length !== 24){
+    const isLoggedin = localStorage.getItem("refreshToken")
+    if(isLoggedin){
         return <Navigate to = {props.route} replace />
     }
     return props.child ? props.child : <Outlet />
